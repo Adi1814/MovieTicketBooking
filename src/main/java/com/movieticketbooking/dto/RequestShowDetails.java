3617bf7182b15e13951eef838c6351d53e54cc1f
Aditi Mishra <adiiii24o4@gmail.com>
2023-10-15 19:59:21 +0530

    new

--- src/main/java/com/movieticketbooking/dto/RequestShowDetails.java
+++ src/main/java/com/movieticketbooking/dto/RequestShowDetails.java
@@ -8,6 +8,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RequestShowDetails {
-    private String names;
+    private String name;
     private String id;
 }
